18ad528a8848af25ccdbc6c30341d317aa24c3af
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2020-10-11 10:25:40 -0300

    adicionando login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 
 const user = require('./user.route');
+const session = require('./session.route');
 
 const routes = new Router();
 
 routes.use('/users', user);
+routes.use('/auth', session);
 
 module.exports = routes;
