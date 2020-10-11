a8053403b9204aa58a28ea7ca879c544e44f0405
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2020-10-11 16:48:34 -0300

    criando middleware para verificar o token

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 
 const ChatController = require('../controllers/chatController');
+const authMiddleware = require('../middlewares/general/authMiddleware')
 
 const routes = new Router();
 
-routes.get('/', ChatController.getAll);
+routes.get('/', authMiddleware, ChatController.getAll);
 
 module.exports = routes;
