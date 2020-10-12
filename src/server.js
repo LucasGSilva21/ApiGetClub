d7bd1c310014f3a139bc17a7d39dc7400c34c884
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2020-10-12 19:48:52 -0300

    removendo comentarios

--- src/server.js
+++ src/server.js
@@ -13,22 +13,17 @@ const server = require('http').createServer(app);
 const socket = require('socket.io')(server);
 
 socket.on('connection', async (socket) => {
-    //console.log('user connected');
-    //console.log(socket.handshake.query);
     let sender = socket.handshake.query.sender;
     let addressee = socket.handshake.query.addressee;
     
     let messages = await getAllMessage(sender, addressee);
-    //console.log(messages);
 
     socket.emit('previousMessages', messages);
 
     socket.on('sendMessage', async data => {
-        //console.log(data);
         let chat = await saveMessage(sender, addressee, data.message);
-        //console.log(chat);
+        
         let message = await getOneMessage(chat._id);
-        //console.log(message);
 
         socket.emit('receivedMessage', message);
     });
