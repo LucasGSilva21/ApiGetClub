6c4c66ad9388d1aa66801569f0575f988a761028
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2020-10-12 21:45:26 -0300

    adicionando validacao para criar usuario

--- src/middlewares/validates/user.validate.js
+++ src/middlewares/validates/user.validate.js
@@ -1,13 +1,30 @@
 const User = require('../../models/user');
+const Yup = require('yup');
 
 module.exports = {
     store: async (req, res, next) => {
-        const { email } = req.body;
+        try {
+            const { email } = req.body;
 
-        if(await User.findOne({ email })){
-            return res.status(400).json({ error: 'User already exists' });
-        }else{
+            if(await User.findOne({ email })){
+                return res.status(400).json({ error: 'User already exists' });
+            }
+
+            const schema = Yup.object().shape({
+                name: Yup.string().required(),
+                email: Yup.string().email().required(),
+                password: Yup.string().required(),
+                cpf: Yup.string().required(),
+                cnpj: Yup.string().required(),
+                fantasyName: Yup.string().required(),
+                service: Yup.string().required(),
+            });
+
+            await schema.validate(req.body);
+      
             next();
+        } catch (e) {
+            res.status(400).json({ error: e.message });
         }
     },
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ const { Router } = require('express');
 
 const user = require('./user.route');
 const session = require('./session.route');
-const chat = require('./chat.route');
 const service = require('./service.route');
 const star = require('./star.route');
 
@@ -10,7 +9,6 @@ const routes = new Router();
 
 routes.use('/user', user);
 routes.use('/auth', session);
-routes.use('/chat', chat);
 routes.use('/service', service);
 routes.use('/star', star);
 
